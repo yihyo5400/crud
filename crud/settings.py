bb62fb022151b9377e37dca9d933c904f0d096b7
yihyo5400 <yihyo5400@naver.com>
2019-07-21 14:47:40 +0900

    final

--- crud/settings.py
+++ crud/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'svrj!$zynm6hu_&+%rr#jh)v)hip)%
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
